0ea41e9817e84e80f94068f193ed456a8ed48db3
r.pouya <rpouya@hotmail.com>
2022-11-09 16:41:43 +0330

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "../src/pages/Details";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./components/SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <Link to="/">
+          <h1>Adopt Me!</h1>
+        </Link>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/index" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
